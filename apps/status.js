a9f3bfb6f7a7e7550c427934200ed209c87773c8
CakmLexi <calmlexi@hotmail.com>
2024-07-02 15:18:03 +0800

    fix: 修正新版本错误

--- apps/status.js
+++ apps/status.js
@@ -1,6 +1,6 @@
 import moment from 'moment'
 import Config from '../lib/config.js'
-import { redis, Bot, App, Cfg } from '#Karin'
+import { redis, Bot, Cfg, plugin } from '#Karin'
 
 let recv_count = ''
 let send_count = ''
@@ -20,30 +20,36 @@ if (Config.Config.status) {
   Bot.on(fnc_key, () => redis.incr(fnc_count))
 }
 
-function init () {
-  // 获取当前时间 格式为 YYYY-MM-DD
-  const time = moment().format('YYYY-MM-DD')
-  recv_count = recv_key + `:${time}`
-  send_count = send_key + `:${time}`
-  fnc_count = fnc_key + `:${time}`
-}
+export class Restart extends plugin {
+  constructor () {
+    super({
+      name: '状态统计',
+      dsc: '统计机器人收发消息数量，插件触发数量等',
+      priority: 100000,
+      rule: [
+        {
+          reg: /^#状态$/,
+          fnc: 'status',
+        },
+      ],
+      task: [
+        {
+          name: '每天0点初始化统计数据',
+          cron: '0 0 * * *',
+          fnc: 'init',
+        },
+      ],
+    })
+  }
 
-init()
+  init () {
+    // 获取当前时间 格式为 YYYY-MM-DD
+    const time = moment().format('YYYY-MM-DD')
+    recv_count = recv_key + `:${time}`
+    send_count = send_key + `:${time}`
+    fnc_count = fnc_key + `:${time}`
+  }
 
-const app = App.init({
-  name: '状态统计',
-  dsc: '统计机器人收发消息数量，插件触发数量等'
-})
-
-app.cron({
-  name: '每天0点初始化统计数据',
-  cron: '0 0 * * *',
-  fnc: init
-})
-
-app.reg({
-  reg: /^#状态$/,
-  fnc: 'status',
   async status () {
     const all = [
       '------机器人状态------',
@@ -57,30 +63,33 @@ app.reg({
       '------本月统计------',
       `发送消息：${await this.getCount(send_key)}次`,
       `插件触发：${await this.getCount(fnc_key)}次`,
-      `收到消息：${await this.getCount(recv_key)}次`
+      `收到消息：${await this.getCount(recv_key)}次`,
     ]
 
     this.reply(all.join('\n'))
-  },
+  }
+
   MB () {
-    let MB = process.memoryUsage().heapUsed / 1024 / 1024
+    let MB = process.memoryUsage().rss / 1024 / 1024
     MB = MB.toFixed(2)
     return MB
-  },
+  }
+
   uptime () {
     let uptime = process.uptime()
-    let [hour, minute] = [3600, 60].map(unit => {
-      let value = Math.floor(uptime / unit)
+    const [hour, minute] = [3600, 60].map(unit => {
+      const value = Math.floor(uptime / unit)
       uptime %= unit
       return value
     })
     return `${hour}小时${minute}分钟`
-  },
+  }
+
   /** 获取30天的消息数量 */
   async getCount (key) {
     let num = 0
     /** 对于30天的统计，预先生成所有可能的键 */
-    // eslint-disable-next-line no-unused-vars
+
     const keys = Array.from({ length: 30 }, (_, i) => {
       const date = moment().subtract(i, 'days').format('YYYY-MM-DD')
       return `${key}:${date}`
@@ -92,6 +101,4 @@ app.reg({
 
     return num
   }
-})
-
-export const status = app.plugin(app)
+}
