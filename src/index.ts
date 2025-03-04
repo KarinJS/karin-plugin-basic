bf27755cbb24647998fee41b89e1cb30f8b7c6fe
时瑾 <74231782+sj817@users.noreply.github.com>
2025-03-05 01:29:45 +0800

    fix: build error

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ import { logger } from 'node-karin'
 import { info } from '@/root'
 
 /** 请不要在这编写插件 不会有任何效果~ */
-logger.info(`${logger.violet(`[插件:${info.version}]`)} ${logger.green(info.name)} 初始化完成~`)
+logger.info(`${logger.violet(`[插件:${info.version}]`)} ${logger.green(info.pkg.name)} 初始化完成~`)
