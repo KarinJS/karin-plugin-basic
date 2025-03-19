a602dd3469a4b8072393014fa2585ff1d61922e5
瑜笙 <2624367622@qq.com>
2025-03-19 16:11:33 +0800

    fix: 删除多余文件

--- dist/apps/exit.d.ts
+++
@@ -1,5 +0,0 @@
-import * as node_karin from 'node-karin';
-
-declare const exit: node_karin.Command<keyof node_karin.MessageEventMap>;
-
-export { exit };
--- dist/apps/status.d.ts
+++
@@ -1,5 +0,0 @@
-import * as node_karin from 'node-karin';
-
-declare const status: node_karin.Command<keyof node_karin.MessageEventMap>;
-
-export { status };
